adc78b1fbf0b4630502f57957d3c32ce724d7cc9
yagnesh parekh <yagneshparekh@rocketmail.com>
2021-11-07 21:45:01 -0500

    create a nextISSTimesForMyLocation func which return promise by calling other functions

--- iss_promised.js
+++ iss_promised.js
@@ -4,8 +4,23 @@ const fetchMyIP = function() {
   return request('https://api.ipify.org?format=json');
 }
 
-const fetchCoordsByIP = function(ip){
+const fetchCoordsByIP = function(result){
+  let ip = JSON.parse(result)['ip']
   return request(`https://api.freegeoip.app/json/${ip}?apikey=${APIKEY}`)
 }
 
-module.exports = {fetchMyIP, fetchCoordsByIP};
+const fetchISSFlyOverTimes = function(result){
+  const {latitude, longitude} = JSON.parse(result)
+  return request(`https://iss-pass.herokuapp.com/json/?lat=${latitude}&lon=${longitude}`);
+}
+
+const nextISSTimesForMyLocation = function() {
+  return fetchMyIP()
+  .then(fetchCoordsByIP)
+  .then(fetchISSFlyOverTimes)
+  .then(result => {
+   let body =  JSON.parse(result);
+   return body.response
+  })   
+}
+module.exports = {nextISSTimesForMyLocation};
