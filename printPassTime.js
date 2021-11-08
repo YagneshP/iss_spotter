e539b0880039912ddf3288e90ff506acc72ce0ff
yagnesh parekh <yagneshparekh@rocketmail.com>
2021-11-07 21:43:44 -0500

    modularize printPassTime function

---
+++ printPassTime.js
@@ -0,0 +1,10 @@
+const printPassTime =  function(passTimes) {
+  for (const pass of passTimes) {
+    const dateTime = new Date(0);
+    dateTime.setUTCSeconds(pass.risetime);
+    const duration = pass.duration;
+    console.log(`Next pass at ${dateTime} for ${duration} seconds!`);
+  }
+};
+
+module.exports = printPassTime;
