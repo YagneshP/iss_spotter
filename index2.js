913a40ed013e8e6b35c83693d9b83de6757894cc
yagnesh parekh <yagneshparekh@rocketmail.com>
2021-11-07 21:49:06 -0500

    handling error

--- index2.js
+++ index2.js
@@ -2,4 +2,5 @@ const {nextISSTimesForMyLocation} = require('./iss_promised');
 const printPassTime = require('./printPassTime');
 
 nextISSTimesForMyLocation()
-.then(result => printPassTime(result));
+.then(result => printPassTime(result))
+.catch(error => console.log(`It didn't work :`, error.message));
